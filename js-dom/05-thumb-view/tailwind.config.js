90ba59712a233afcfd1bf60d3e51bd67bf6f6e68
Jeya-rosini <jeya.rosini@gmail.com>
2024-01-09 05:51:50 +0530

    feat: css apply as overview

--- js-dom/05-thumb-view/tailwind.config.js
+++ js-dom/05-thumb-view/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'tektur':['Tektur', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
