3cfa53f028207f5eeb394bdc50ee884c8ad7c79b
Jeya-rosini <jeya.rosini@gmail.com>
2024-01-09 11:34:13 +0530

    fix: dist implement

--- js-dom/05-thumb-view/assets/js/main.js
+++ js-dom/05-thumb-view/assets/js/main.js
@@ -1,15 +1,22 @@
+
+
 const link = document.querySelector("#link");
 const buttonEl = document.querySelector("#button");
 const thumbnailLink = document.querySelector("#thumbnailImage");
 
 // http://img.youtube.com/vi/YouTubeID/ImageFormat.jpg
 
+// https://www.youtube.com/watch?v=OuUqS8Po5ps&list=PL73Obo20O_7ihsIM5K-hHYPrcqkkdQcLa
+
 buttonEl.addEventListener("click", () => {
   const url = link.value;
   const splitUrl = url.split("v=");
   const videoId = splitUrl[1];
-//   console.log(videoId);
+  console.log(videoId);
   const finalUrl = `http://img.youtube.com/vi/${videoId}/maxresdefault.jpg`;
 //   console.log(finalUrl);
-  thumbnailLink.src = finalUrl;
+      thumbnailLink.src = finalUrl; 
+  
 });
+
+
