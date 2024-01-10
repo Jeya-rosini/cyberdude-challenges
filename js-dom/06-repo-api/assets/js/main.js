aa55571c3e7b8c90e10c4a4cd4999037af74d51a
Jeya-rosini <jeya.rosini@gmail.com>
2024-01-10 06:26:40 +0530

    feat: showing the github profile through this api

--- js-dom/06-repo-api/assets/js/main.js
+++ js-dom/06-repo-api/assets/js/main.js
@@ -1,21 +1,42 @@
-const profile = document.querySelector("#profile");
+const allProfile = document.querySelectorAll("#profile");
 const xhr = new XMLHttpRequest();
 
-// console.dir(profile.pathname);
+const userName = [
+  "esakki2104prsnl",
+  "vk2401",
+  "suriyamassmsd",
+  "riyaz1000",
+  "hema-venkat3",
+  "yrd369",
+  "mushkir",
+  "bearcin46",
+  "dineshdevelope",
+  "jeya-rosini",
+  "swethadsalvatore",
+  "mshajid",
+  "muthuakalya",
+  "vedhatech002",
+  "muthukumarimoorthi",
+  "sharif-22",
+  "danielace1",
+  "gayathrihg",
+  "kishorekumar-kp",
+];
+
+// console.log(userName);
 
 xhr.onreadystatechange = function () {
-    console.log(this);
- 
-    if (this.readyState === 4 && this.status === 200) {
-        // console.log(xhr.responseText);
-        const userName = profile.pathname.split("/");
-        const userId = userName[1];
-        console.log(userId);
-    }
+//   console.log(this);
+
+  if (this.readyState === 4 && this.status === 200) {
+    // console.log(xhr.responseText);
+      allProfile.forEach((userName) => {
+        
+          const repoLink = `https://api.github.com/users/${userName}/repos`
+        //   console.log(repoLink);
+    });
+  }
 };
-  
-  xhr.open(
-    "GET",
-    "https://api.github.com/users/jeya-rosini/repos"
-  );
-  xhr.send();
+
+xhr.open("GET", "https://api.github.com/users/<USER-NAME>/repos");
+xhr.send();
