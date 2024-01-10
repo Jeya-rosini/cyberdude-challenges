7ed662f129481f869aa17f8022248c25f42ff4f3
Jeya-rosini <jeya.rosini@gmail.com>
2024-01-10 07:01:10 +0530

    fix: change the table

--- js-dom/05-thumb-view/assets/js/main.js
+++ js-dom/05-thumb-view/assets/js/main.js
@@ -1,5 +1,3 @@
-
-
 const link = document.querySelector("#link");
 const buttonEl = document.querySelector("#button");
 const thumbnailLink = document.querySelector("#thumbnailImage");
@@ -15,18 +13,14 @@ buttonEl.addEventListener("click", () => {
   const videoId = splitUrl[1];
   // console.log(videoId);
   const finalUrl = `http://img.youtube.com/vi/${videoId}/maxresdefault.jpg`;
-//   console.log(finalUrl);
+  //   console.log(finalUrl);
   thumbnailLink.src = finalUrl;
   if (videoId.indexOf("&list=")) {
     const splitPlaylistId = videoId.split("&list=");
     const playlistId = splitPlaylistId[1];
     // console.log(playlistId);
-    const finalLink = `https://i.ytimg.com/vi/${playlistId}/maxresdefault.jpg`
+    const finalLink = `https://i.ytimg.com/vi/${playlistId}/maxresdefault.jpg`;
     console.log(finalLink);
-  }
- 
+    thumbnailLink.src = finalLink;
+  }           
 });
-
-
-
- 
--- js-dom/readme.md
+++ js-dom/readme.md
@@ -23,3 +23,4 @@ This internship is provided by [CyberDude Networks Pvt. Ltd.](https://youtube.co
 | 03   | [03-json-file](03-json-file) | [Youtube] ()  |
 | 04   | [04-api](04-api)              | [Youtube] ( ) |
 | 05   | [05-thumb-view](05-thumb-view) | [Youtube] ( ) |
+| 06   | [GitHub API](06-repo-api)      | [Youtube] ( ) |
