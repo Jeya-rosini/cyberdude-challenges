602aa9dc68134284dbd1b1793dcab6f8124fe83b
Jeya-rosini <jeya.rosini@gmail.com>
2024-01-30 16:46:11 +0530

    feat:dynamic value get usign hook param

--- react-js/src/components/form.jsx
+++ react-js/src/components/form.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 export default function suggestion() {
+
     function suggestion(formData) {
         const GuestName = formData.get("GuestName");
         console.log(GuestName);
@@ -9,12 +10,12 @@ export default function suggestion() {
         <div className="m-4 p-2">
              <form action={suggestion}>
                 <div className="space-y-2 mb-2">
-                    <label for="GuestName" className="font-bold font-cabin tracking-wider text-xl">Your Name</label>
+                    <label for="GuestName" className="font-semibold font-cabin tracking-wider text-2xl">Your Name</label>
                 <input name="GuestName" placeholder="Enter your name" className="w-full outline-none rounded p-2"/>
                 </div>
                 <div className="space-y-2 mb-2">
-                    <lable for="message" className="font-bold font-cabin tracking-wider text-xl">Your Message</lable>
-                <textarea className="p-2 w-full rounded h-56">Enter your suggestions here</textarea>
+                    <lable for="message" className="font-semibold font-cabin tracking-wider text-2xl">Your Message</lable>
+                <textarea className="p-2 w-full rounded h-56" placeholder="Enter your suggestion"></textarea>
                 </div>
                 <div className="space-y-2">
                 <button type="submit" className="p-2 bg-[#ffd369] m-2 rounded font-semibold font-cabin tracking-wide hover:cursor-pointer hover:bg-white">Submit</button>
--- react-js/src/main.jsx
+++ react-js/src/main.jsx
@@ -30,6 +30,10 @@ const router = createBrowserRouter([
                 path: '/contact',
                 element: <ContactPage/>,
             },
+            {
+                path: '/contact/:name',
+                element:<ContactPage/>
+            },
         ],
         errorElement:<ErrorPage/>
     },
