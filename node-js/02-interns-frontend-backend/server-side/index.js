3e077e1205969aa1d818be04b47fc185fb6f442f
Jeya-rosini <jeya.rosini@gmail.com>
2024-02-20 01:21:38 +0530

    feat:server-side pushed

--- node-js/02-interns-frontend-backend/server-side/index.js
+++ node-js/02-interns-frontend-backend/server-side/index.js
@@ -4,7 +4,7 @@ const internsData = require('./data/interns.json');
 const serverPort = "2528"
 
 httpServer.createServer((request, response) => {
-    console.log("The server port is created " + serverPort);
-    response.write("Hello World!!, This is Rosini")
+    // console.log("The server port is created " + serverPort);
+    response.write(JSON.stringify(internsData))
     response.end()
 }).listen(serverPort)
